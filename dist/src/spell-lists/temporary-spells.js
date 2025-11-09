c223ba49d77b0315e7c28956f60ee1c87f489219
emallson <emallson@emallson.net>
2025-11-09 11:39:46 -0500

    do not add overrides with invalid spell ids

--- dist/src/spell-lists/temporary-spells.js
+++ dist/src/spell-lists/temporary-spells.js
@@ -4,15 +4,18 @@ export default async function temporarySpells(dbc, otherSpells) {
     const spellEffect = await dbc.loadTable("SpellEffect", "SpellID");
     return otherSpells.flatMap((spell) => {
         const effects = spellEffect.getAll(spell.id);
-        return effects
+        return (effects
             .filter((effect) => effect.EffectAura === EffectType.OVERRIDE_ACTIONBAR_SPELLS ||
-            effect.EffectAura === EffectType.OVERRIDE_ACTIONBAR_SPELLS_TRIGGERED)
+            effect.EffectAura ===
+                EffectType.OVERRIDE_ACTIONBAR_SPELLS_TRIGGERED)
+            // some override effects have been left in the dbc that don't *do* anything
+            .filter((effect) => effect.EffectBasePoints > 0 || effect.EffectBasePointsF > 0)
             .map((effect) => ({
             id: effect.EffectBasePointsF === 0
                 ? effect.EffectBasePoints
                 : effect.EffectBasePointsF,
             grantedBy: spell.id,
             type: SpellType.Temporary,
-        }));
+        })));
     });
 }
--- src/spell-lists/temporary-spells.ts
+++ src/spell-lists/temporary-spells.ts
@@ -26,22 +26,30 @@ export default async function temporarySpells(
   return otherSpells.flatMap((spell) => {
     const effects = spellEffect.getAll(spell.id);
 
-    return effects
-      .filter(
-        (effect) =>
-          effect.EffectAura === EffectType.OVERRIDE_ACTIONBAR_SPELLS ||
-          effect.EffectAura === EffectType.OVERRIDE_ACTIONBAR_SPELLS_TRIGGERED,
-      )
-      .map(
-        (effect) =>
-          ({
-            id:
-              effect.EffectBasePointsF === 0
-                ? effect.EffectBasePoints
-                : effect.EffectBasePointsF,
-            grantedBy: spell.id,
-            type: SpellType.Temporary,
-          }) satisfies TemporarySpell,
-      );
+    return (
+      effects
+        .filter(
+          (effect) =>
+            effect.EffectAura === EffectType.OVERRIDE_ACTIONBAR_SPELLS ||
+            effect.EffectAura ===
+              EffectType.OVERRIDE_ACTIONBAR_SPELLS_TRIGGERED,
+        )
+        // some override effects have been left in the dbc that don't *do* anything
+        .filter(
+          (effect) =>
+            effect.EffectBasePoints > 0 || effect.EffectBasePointsF > 0,
+        )
+        .map(
+          (effect) =>
+            ({
+              id:
+                effect.EffectBasePointsF === 0
+                  ? effect.EffectBasePoints
+                  : effect.EffectBasePointsF,
+              grantedBy: spell.id,
+              type: SpellType.Temporary,
+            }) satisfies TemporarySpell,
+        )
+    );
   });
 }
